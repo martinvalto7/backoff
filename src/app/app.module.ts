418bff98fba52d5af8b125789919a9fca30fec20
martinvalto <martin.gobin1@gmail.com>
2019-12-16 11:54:51 +0100

    configuration angular material 2.0(toolbar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { MatTableModule } from '@angular/material/table';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatFormFieldModule,
-    MatTableModule
+    MatTableModule,
     
   ],
   providers: [],
